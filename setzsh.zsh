02611183d5e4330241f14f76699cc347d7f69ed8
Forest Walker <forest@tadf.net>
2020-10-27 14:01:11 -0700

    Edited paths

--- setzsh.zsh
+++ setzsh.zsh
@@ -5,11 +5,11 @@ fi
 
 cp ./.zshrc $HOME
 if [ $1 = "mac" ]; then
-	cp ./mac/.zsh_aliases $HOME
+	cp './mac/.zsh_aliases' $HOME
 elif [ $1 = "apt" ]; then
-	cp ./apt/.zsh_aliases $HOME
+	cp './apt/.zsh_aliases' $HOME
 elif [ $1 = "pacman" ]; then
-	cp ./pacman/.zsh_aliases $HOME
+	cp './pacman/.zsh_aliases' $HOME
 fi
 cp ./.zsh_paths $HOME
 cp ./.vimrc $HOME
