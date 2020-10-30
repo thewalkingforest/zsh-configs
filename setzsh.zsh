e52123bb43ffe0c17b146ca805275371aaab0a7c
Forest Walker <forest@tadf.net>
2020-10-30 13:20:28 -0700

    Added changes for mac arguments

--- setzsh.zsh
+++ setzsh.zsh
@@ -3,13 +3,17 @@ if [ $# -ne 1 ]; then
 	exit 1
 fi
 
-cp ./.zshrc $HOME
 if [ $1 = "mac" ]; then
 	cp './mac/.zsh_aliases' $HOME
+	cp './mac/.zshrc' $HOME
 elif [ $1 = "apt" ]; then
 	cp './apt/.zsh_aliases' $HOME
+	cp ./.zshrc $HOME
 elif [ $1 = "pacman" ]; then
 	cp './pacman/.zsh_aliases' $HOME
+	cp ./.zshrc $HOME
+else
+	echo "Package manager not supported"
 fi
 cp ./.zsh_paths $HOME
 cp ./.vimrc $HOME
